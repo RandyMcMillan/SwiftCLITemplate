ba8f0686cb2794705d86db2f60eb24e95a2e6589
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-01-16 13:41:05 -0500

    Sources/MainCLI.swift:repeat

--- Sources/MainCLI.swift
+++ Sources/MainCLI.swift
@@ -4,13 +4,26 @@ import ArgumentParser
 import Files
 import SwiftFigletKit
 
-
 @main
-struct MainCLI: ParsableCommand {
-    @Flag(name: [.customLong("flag"), .customShort("f")],
-          help: "Just a flag, use with --flag or -f")
-    var flag = false
-    
-    func run() throws {
+struct Repeat: ParsableCommand {
+    @Flag(help: "Include a counter with each repetition.")
+    var includeCounter = false
+
+    @Option(name: .shortAndLong, help: "The number of times to repeat 'phrase'.")
+    var count: Int?
+
+    @Argument(help: "The phrase to repeat.")
+    var phrase: String
+
+    mutating func run() throws {
+        let repeatCount = count ?? .max
+
+        for i in 1...repeatCount {
+            if includeCounter {
+                print("\(i): \(phrase)")
+            } else {
+                print(phrase)
+            }
+        }
     }
 }
